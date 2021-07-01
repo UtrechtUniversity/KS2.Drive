75ae8b2714c12d6bbc595bbb4c24c2a67446ed84
Osman Kaya <osman.kaya@ru.nl>
2021-07-01 07:44:09 +0200

    renamed to RDR Drive

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Radboud Research Drive")]
+[assembly: AssemblyTitle("RDR drive")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Radboud Research Drive")]
+[assembly: AssemblyProduct("RDR drive")]
 [assembly: AssemblyCopyright("Copyright ©KaliConseil 2017-2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
